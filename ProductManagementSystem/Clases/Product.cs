4cb871ab95bb2046714752b5fcca28c1953b27ab
SaraEOlivera <saraeolivera@gmail.com>
2025-05-22 10:44:59 -0300

    Se crea proyecto Pfo Testing y las 2 clases Product y ProductManager

---
+++ ProductManagementSystem/Clases/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem
+{
+
+    public enum ProductCategory
+    {
+        Electronica,
+        Alimentos
+        
+    }
+    public class Product
+    {
+        private decimal precio;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price
+        {
+            get { return precio; }
+            set {
+                if (value >= 0)
+                {
+                    precio = value;
+                }
+                else {
+                    throw new ArgumentException("El precio no puede ser negativo");
+                }
+            }
+        }
+
+        public ProductCategory Category { get; set; }                        
+    }
+
+}
